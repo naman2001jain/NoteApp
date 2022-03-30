c9881aa17265c723e45bcfebe552e06c19eaa154
naman jain <naman2001jain@gmail.com>
2022-03-31 01:01:58 +0530

    edited the url of end points...

--- NoteApp/Services/ApiCallers.swift
+++ NoteApp/Services/ApiCallers.swift
@@ -6,7 +6,7 @@ struct ApiCallers{
     
     static var shared = ApiCallers();
     func fetchNotes(){
-        AF.request("http://localhost:8080/fetch").response{ res in
+        AF.request("http://localhost:8080/route/fetch").response{ res in
             print(res.data)
             let data = String(data: res.data ?? Data(count: 2) ,encoding: .utf8)
             self.delegate?.updateArray(newArray: data ?? "nothing")
@@ -14,14 +14,14 @@ struct ApiCallers{
     }
     
     func addNotes(title: String, note: String, date: String){
-        AF.request("http://localhost:8080/create", method: .post, encoding: URLEncoding.httpBody, headers: ["title": title, "note": note, "date": date])
+        AF.request("http://localhost:8080/route/create", method: .post, encoding: URLEncoding.httpBody, headers: ["title": title, "note": note, "date": date])
             .responseDecodable(of: Note.self) { response in
                 print("Note added to the database successfully!")
         }
     }
     
     func updateNotes(title: String, note: String, date: String, id: String){
-        AF.request("http://localhost:8080/update", method: .patch, encoding: URLEncoding.httpBody, headers: ["title": title, "note": note, "date": date, "id": id])
+        AF.request("http://localhost:8080/route/update", method: .patch, encoding: URLEncoding.httpBody, headers: ["title": title, "note": note, "date": date, "id": id])
             .responseDecodable(of: Note.self){ response in
                 
                 print(response)
@@ -29,7 +29,7 @@ struct ApiCallers{
     }
     
     func deleteNote(id: String){
-        AF.request("http://localhost:8080/delete", method: .delete, encoding: URLEncoding.httpBody, headers: ["id": id])
+        AF.request("http://localhost:8080/route/delete", method: .delete, encoding: URLEncoding.httpBody, headers: ["id": id])
             .responseDecodable(of: Note.self){ response in
                 
                 print(response)
